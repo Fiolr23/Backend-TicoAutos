6067793ed5d8e0f1a82686798522cc12a35d7a29
Nayely Vega <nayevm04@gmail.com>
2026-03-16 00:42:28 -0600

    chore(server): actualizar configuración del servidor y dependencias

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
+const vehicleRoutes = require("./routes/vehicleRoutes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/vehicles", vehicleRoutes);
 
 const PORT = process.env.PORT || 3000;
 
